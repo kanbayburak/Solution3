460349a4b27447b6d3136634531fa7f616499ea3
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2024-08-23 17:37:34 +0300

    1. Kurs 43-44-45-46. Bölüm

---
+++ Burak.OgrenciTakip.Common/Messages/Messages.cs
@@ -0,0 +1,68 @@
+﻿using DevExpress.XtraEditors;
+using System.Windows.Forms;
+
+namespace Burak.OgrenciTakip.Common.Messages
+{
+    public class Messages
+    {
+
+
+        //bir function oluşturduk 
+        public static void HataMesaji(string hataMesaji)
+        {
+            XtraMessageBox.Show(hataMesaji, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error); //error burada error iconu gelmesini sağlıyor 
+        }
+
+        public static void UayriMesaji(string uayariMesaji)
+        {
+            XtraMessageBox.Show(uayariMesaji, "Uyar", MessageBoxButtons.OK, MessageBoxIcon.Information); //Information burada Information iconu gelmesini sağlıyor 
+        }
+
+
+        public static DialogResult EvetSeciliEvetHayir(string mesaj, string baslik)
+        {
+            return XtraMessageBox.Show(mesaj, baslik, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);  //MessageBoxDefaultButton.Button1 seçerek yes seçeneğinin seçili olarak gelmesini belirttik
+        }
+        public static DialogResult HayirSeciliEvetHayir(string mesaj, string baslik)
+        {
+            return XtraMessageBox.Show(mesaj, baslik, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);  //MessageBoxDefaultButton.Button2 seçerek no seçeneğinin seçili olarak gelmesini belirttik
+        }
+        public static DialogResult EvetSeciliEvetHayirIptal(string mesaj, string baslik)
+        {
+            return XtraMessageBox.Show(mesaj, baslik, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+        }
+        public static DialogResult SilMesaj(string kartAdi)
+        {
+            return HayirSeciliEvetHayir($"Seçtiğiniz {kartAdi} Silinecektir. Onaylıyor musunuz?", "Silme Onayı");
+        }
+
+        public static DialogResult KapanisMesaj()
+        {
+            return EvetSeciliEvetHayirIptal("Yapılan Değişiklikler Kaydedilsin mi?", "Çıkış Onay");
+        }
+
+        public static DialogResult KayitMesaj()
+        {
+            return EvetSeciliEvetHayir("Yapılan Değişiklikler Kaydedilsin mi?", "Kayıt Onay");
+        }
+        public static void KartSecmemeUyariMesaji()
+        {
+            UayriMesaji("Lütfen Bir Kart Seçiniz.");
+        }
+        public static void MukerrerKayitHataMesaji(string alanAdi)
+        {
+            HataMesaji($"Girmiş Olduğunuz {alanAdi} Daha Önce Kullanılmıştır.");
+        }
+
+        public static void HataliVeriMesaji(string alanAdi)
+        {
+            HataMesaji($"{alanAdi} Alanına Geçerli Bir Değer Girmelisiniz.");
+        }
+
+        public static DialogResult TabloExportMesaji(string dosyaFormati)
+        {
+            return EvetSeciliEvetHayir($"İlgili Tablo, {dosyaFormati} Olarak DışarıAktarılacaktır. Aktarım Onay?", "Onaylıyor Musunuz?");
+        }
+
+    }
+}
---
+++ BurakOgrenciTakip/Base/UnitOfWork.cs
@@ -0,0 +1,110 @@
+﻿using Burak.OgrenciTakip.Common.Messages;
+using BurakOgrenciTakip.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurakOgrenciTakip.Base
+{
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : class
+    {
+
+        #region Variables
+        private readonly DbContext _context;
+        #endregion
+
+        //constrcution oluştur. save işlemini yapabilmemiz için. Bu context i tyerel değişkenlerde kullanabilmemiz için _context oluşturmamız lazımdı üstte oluşturduk
+        public UnitOfWork(DbContext context)
+        {
+            if (context == null) return;
+            {
+                _context = context;
+            }
+        }
+        //bu sayede UnitOfWorks den Repository e ulaşabileceğiz.
+        public IRepository<T> Rep => new Repository<T>(_context);
+
+        //hataları ayıklayabilmemiz için save blogunun arasında try chach kullanmalıyız 
+        public bool Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+
+                var sqlEx = (SqlException)ex.InnerException?.InnerException;
+                if (sqlEx == null)
+                {
+                    //common katmamnından message ı çekmemiz lazım //Bunu referanslar kısmından cı-ommon kısmını seçtik o şekilde implemante ettik
+                    Messages.HataMesaji(ex.Message);
+                    return false;
+                }
+                switch (sqlEx.Number)
+                {
+                    case 208:
+                        Messages.HataMesaji("İşlem Yapmak İstediğiniz Tablo Veri Tabanında BUlunamadı.");
+                        break;
+                    case 547:
+                        Messages.HataMesaji("Seçilen Kartın İşlme Görmüş Hareketleri var Kart Silinemez.");
+                        break;
+                    case 2601:
+                    case 2627:
+                        Messages.HataMesaji("Girmiş Olduğunuz Id Daha Önce Kullanılmıştır.");
+                        break;
+                    case 4060:
+                        Messages.HataMesaji("İŞlem Yapmak İstediğiniz VeriTabanı Sunucuda Bulunamadı.");
+                        break;
+                    case 18456:
+                        Messages.HataMesaji("Server'a Bağlanılmak istenen Kullanıcı Adı ve Şifre Hatalıdır.");
+                        break;
+                    default:
+                        Messages.HataMesaji(sqlEx.Message);
+                        break;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Messages.HataMesaji(ex.Message);
+                return false;
+
+            }
+            return true;
+        }
+
+        #region Dispose
+        private bool _disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+
+            Dispose(true);
+            GC.SuppressFinalize(this);
+
+            #endregion
+        }
+    }
+}
