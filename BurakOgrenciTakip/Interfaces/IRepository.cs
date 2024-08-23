0ae52110d62eede48b9d345cf76fa17caff116d9
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2024-08-23 15:13:20 +0300

    1.Kurs 41-42. Videolar

---
+++ BurakOgrenciTakip/Interfaces/IRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurakOgrenciTakip.Interfaces
+{
+    //T nin aslında bir anlamı yok farklı bir şeyde yazılabilirdi. Anlamı ise herhangi bir tipde bir Entity gelecek olduğunu belirtmiş oluyoruz   
+    //IDisposable ile implemente ettik bunun nedeni bu interface i implemente eden class lar işlmeleri bittikten sonra hafızadan siliyor işlemine tabi tutmuş oluyoruz
+    public interface IRepository<T> : IDisposable where T : class//T nin tipini tanımlıyoruz (where T: class > demek istiyorki sana gelen T ler sadece class olacak, bu classlarda enetitylerimizdir.)
+    {
+        //arayüzümüzü oluşturuyoruz
+        //amacımız şu Insert, CRUD işlemlerini tek bir class da toplayıp tüm enetitylerimiz buraya çağırıp işlemleri gerçekleştiriyoruz.
+        void Insert(T entity);
+        void Insert(IEnumerable<T> entities);  //birden çok enetity gönderme durumunda ınsert işlemlerini yap
+
+        void Update(T entity); //T enetity göderiyorum bunu güncelle
+        void Update(T entity, IEnumerable<string> fields); //liste türünde bir enetity T gönderebilirim, bu enetity i değilde bu entityde değişen alanlarını update et demiş olduk 
+
+        void Update(IEnumerable<T> entities); //T türünden entity gönderebilirim, bunları update edeceksin 
+
+        void Delete(T entity); //T enetity göderiyorum bunu sil
+        void Delete(IEnumerable<T> entities); //liste türünde bir enetity T gönderebilirim, bu göndermiş olduğum entityleri de sil
+
+        //bir değer dönecek ama bu değerin türünü bilmiyoruz.T tipinde sorgular alacağız ve bu sorgular sonucunda bu yüzden datatransferobject tipinde veriler dönecek. Tipini bilmediğimiz için IResult yazdık
+        //TReslt şu demek ben sana sorgulama anında tipini vereceğim 
+        //2. yazan Expression T tipinde değer alıyor, bu gelen T tipi filterlar arasında dolaşıyor ihtiyacımız olanları seçiyor TResult olarak geri dödürüyor 
+        TResult Find<TResult>(Expression<Func<T, bool>> filter, Expression<Func<T, TResult>> selector);
+
+        //sorgu yapılacak IQueryable ile
+        //T türünü barından bir filter göndereceğiz, bu filtre ile sen git gerekli sorgulamaları yap, eğer sorgu sonucunda true dönüyorsa kayıt vardır demektir ve bunları geriye gönder.
+        //yine TResult türünde olacak bu da sorgu anında belli olacak 
+        IQueryable<TResult> Select<TResult>(Expression<Func<T, bool>> filter, Expression<Func<T, TResult>> selector);
+
+    }
+}
---
+++ BurakOgrenciTakip/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BurakOgrenciTakip.Interfaces
+{
+    //okul entity si üzerinden çeşitli update, delete, insert işlemleri yapıldı ve sonrasında Repository e gönderiyoruz ancak Repository e göndermemiz demek bunu database e gönderiyoruz anlamına geliyor. çünkü herhangi bir save işlemi yapıp database e göndermiyor. 
+    //bunları tek tek repositorye gönderip save işlemi yapabiliriz ama biz böyle yapmayacağız toplu olarak işlmeleri yapıp save işlemini Unitofwork yapacak. 
+    public interface IUnitOfWork<T> : IDisposable where T : class // demek istiyorki sana gelen T ler sadece class olacak, bu classlarda enetitylerimizdir.
+    {
+        //repostryden gelen talepleri database e gönderme işlemi
+        IRepository<T> Rep { get; } // repostory dekie tüm functionlara ulaşma şekli
+        bool Save(); //kaydedildiyse true değilse false
+    }
+}
