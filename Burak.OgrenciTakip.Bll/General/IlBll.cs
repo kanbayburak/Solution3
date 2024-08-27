c8f0eb54162d1c53f98240b6633ee838e7d0394c
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2024-08-27 14:33:51 +0300

    1. Kurs 77. Video

---
+++ Burak.OgrenciTakip.Bll/General/IlBll.cs
@@ -0,0 +1,63 @@
+﻿using Burak.OgrenciTakip.Bll.Base;
+using Burak.OgrenciTakip.Bll.Interfaces;
+using Burak.OgrenciTakip.Common.Enums;
+using Burak.OgrenciTakip.Data.Contexts;
+using Burak.OgrenciTakip.Model.Entities;
+using Burak.OgrenciTakip.Model.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Windows.Forms;
+
+namespace Burak.OgrenciTakip.Bll.General
+{
+    public class IlBll : BaseBll<Il,OgrenciTakipContext>, IBaseGenelBll
+    {
+        public IlBll() { }
+        public IlBll(Control ctrl):base(ctrl) { }
+
+        public BaseEntity Single(Expression<Func<Il, bool>> filter)
+        {
+            return BaseSingle(filter, x => new Il
+            {
+                Id = x.Id,
+                Kod = x.Kod,
+                IlAdi = x.IlAdi,
+                Aciklama = x.Aciklama,
+                Durum = x.Durum
+            });
+        }
+
+        public IEnumerable<BaseEntity> List(Expression<Func<Il, bool>> filter)
+        {
+            return BaseList(filter, x => new Il
+            {
+                Id = x.Id,
+                Kod = x.Kod,
+                IlAdi = x.IlAdi,
+                Aciklama = x.Aciklama
+            }).OrderBy(x => x.Kod).ToList();
+        }
+
+        public bool Insert(BaseEntity entity)
+        {
+            return BaseInsert(entity, x => x.Kod == entity.Kod);
+        }
+
+        public bool UpDate(BaseEntity oldEntity, BaseEntity currentEntity)
+        {
+            return BaseUpdate(oldEntity, currentEntity, x => x.Kod == currentEntity.Kod);
+        }
+
+        public bool Delete(BaseEntity entity)
+        {
+            return BaseDelete(entity, KartTuru.Il);
+        }
+
+        public string YeniKodVer()
+        {
+            return BaseYeniKodVer(KartTuru.Il, x => x.Kod);
+        }
+    }
+}
---
+++ Burak.OgrenciTakip.Bll/General/IlceBll.cs
@@ -0,0 +1,49 @@
+﻿using Burak.OgrenciTakip.Bll.Base;
+using Burak.OgrenciTakip.Common.Enums;
+using Burak.OgrenciTakip.Data.Contexts;
+using Burak.OgrenciTakip.Model.Entities;
+using Burak.OgrenciTakip.Model.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Windows.Forms;
+
+namespace Burak.OgrenciTakip.Bll.General
+{
+    public class IlceBll : BaseBll<Ilce, OgrenciTakipContext>
+    {
+
+        public IlceBll() { }
+        public IlceBll(Control ctrl):base(ctrl) { }
+
+        public BaseEntity Single(Expression<Func<Ilce, bool>> filter)
+        {
+            return BaseSingle(filter, x => x);
+        }
+
+        public IEnumerable<BaseEntity> List(Expression<Func<Ilce, bool>> filter)
+        {
+            return BaseList(filter, x => x).OrderBy(x => x.Kod).ToList();
+        }
+        public bool Insert(BaseEntity entity, Expression<Func<Ilce, bool>> filter)
+        {
+            return BaseInsert(entity, filter);
+        }
+
+        public bool UpDate(BaseEntity oldEntity, BaseEntity currentEntity, Expression<Func<Ilce, bool>> filter)
+        {
+            return BaseUpdate(oldEntity, currentEntity, filter);
+        }
+
+        public bool Delete(BaseEntity entity)
+        {
+            return BaseDelete(entity, KartTuru.Ilce);
+        }
+
+        public string YeniKodVer(Expression<Func<Ilce, bool>> filter)
+        {
+            return BaseYeniKodVer(KartTuru.Ilce, x => x.Kod, filter);
+        }
+    }
+}
